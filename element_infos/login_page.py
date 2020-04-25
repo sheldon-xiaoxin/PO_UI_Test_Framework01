62e5ca6f1959e85eec458179e0439a2c3c1063bb
sheldon <xiaoxinerduo@163.com>
2020-04-25 10:36:04 +0800

    第一次提交 以登录页面实现最原始的pageobject模式

---
+++ element_infos/login_page.py
@@ -0,0 +1,36 @@
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+current_path = os.path.dirname(__file__)
+driver_path = os.path.join(current_path, '../webdiver/geckodriver')
+
+class LoginPage(object):
+    def __init__(self):
+        self.dirver = webdriver.Firefox()
+        self.dirver.implicitly_wait(10)
+        self.dirver.maximize_window()
+        self.dirver.get("http://47.107.178.45/zentao/www/index.php?m=user&f=login")
+        self.username_inputbox = self.dirver.find_element(By.XPATH,'//input[@name="account"]')  #属性===》页面的控件
+        self.password_inputbox = self.dirver.find_element(By.XPATH,'//input[@name="password"]')
+        self.login_button =  self.dirver.find_element(By.XPATH,'//button[@id="submit"]')
+        self.Keeplogin_checkout =  self.dirver.find_element(By.XPATH,'//input[@name="keepLogin[]"]')
+
+    def input_username(self,username):  #方法=====>控件的操作
+        self.username_inputbox.send_keys(username)
+
+
+    def input_password(self,password):
+        self.password_inputbox.send_keys(password)
+
+
+    def click_login(self):
+        self.login_button.click()
+
+
+if __name__ == "__main__":
+    login_page = LoginPage()
+    login_page.input_username('test01')
+    login_page.input_password('newdream123')
+    login_page.click_login()
